e1afb8a61530036b55a5f8c4e55098f1fa920702
leez123 <59280426+leez123@users.noreply.github.com>
2019-12-27 15:26:27 +0100

    Add files via upload

---
+++ Week1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone Project 
+# ### Course 9, IBM Data Science Professional Certificate
+
+# #### *Author: Aleeza LADHA*
+
+# In[3]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[4]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
